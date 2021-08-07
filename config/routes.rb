1b2913602d0b49ae9787a386d2645781b9f0b3b5
inodaii <rag120110@gmail.com>
2021-08-07 05:58:22 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
    get 'books' => 'books#index', as: 'books'
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-
+  root to: 'homes#top'
   post 'books' => 'books#create'
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
